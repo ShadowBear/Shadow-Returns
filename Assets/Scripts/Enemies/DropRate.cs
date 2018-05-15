d3a11fb6698c81f848efab2b12e8d97e3e7a55ea
zadinx2@gmail.com <zadinx2@gmail.com>
2018-05-15 18:33:59 +0200

    Dungeon Assets & Collider

--- Assets/Scripts/Enemies/DropRate.cs
+++ Assets/Scripts/Enemies/DropRate.cs
@@ -13,10 +13,16 @@ public class DropRate : MonoBehaviour {
     [SerializeField]
     private GameObject ep;    
     public int epValue;
+    private Vector3 offset;
 
     //DropRate in Percent 0-1
     [Range(0,1)]
     public float dropRate;
+
+    void Start()
+    {
+        offset = new Vector3(0, 1, 0);
+    }
 
 
     public void DropItem()
@@ -26,7 +32,7 @@ public class DropRate : MonoBehaviour {
             int item = (int)Random.Range(0, drops.Length);
             Instantiate(drops[item], transform.position, transform.rotation);
         }
-        GameObject epObject = Instantiate(ep, transform.position, transform.rotation);
+        GameObject epObject = Instantiate(ep, transform.position + offset, transform.rotation);
         epObject.GetComponent<Experience>().experience = epValue;
     }
 }
---
+++ Assets/Scripts/ObjectScripts/TreasureChest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChest : MonoBehaviour {
+
+    private Animator anim;
+    private bool open = false;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (anim) anim.SetTrigger("Shake");
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E) && Time.timeScale > 0)
+            {
+                open = open ? false : true;
+                if (anim) anim.SetBool("Open", open);
+                //print("E gedrückt");
+            }
+        }
+    }
+}
