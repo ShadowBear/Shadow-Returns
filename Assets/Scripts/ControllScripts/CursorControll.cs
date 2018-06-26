c73969850edb81a53cf12d52b7062112de1e95b7
ShadowBear <lenniesi@web.de>
2018-06-27 00:48:47 +0200

    Arsenal Pack

--- Assets/Scripts/ControllScripts/CursorControll.cs
+++ Assets/Scripts/ControllScripts/CursorControll.cs
@@ -6,9 +6,15 @@ public class CursorControll : MonoBehaviour {
 
     // Use this for initialization
     public Transform playerRotationTrans;
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void Start()
+    {
+        playerRotationTrans = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
         transform.position = Input.mousePosition;
         transform.rotation = Quaternion.Euler(0, 0, -playerRotationTrans.rotation.eulerAngles.y);
 
