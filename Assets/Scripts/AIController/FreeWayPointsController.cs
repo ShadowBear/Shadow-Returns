6d6222d63f575b4fa92d48f2c7efbdf539d9fbbc
ShadowBear <lenniesi@web.de>
2018-07-30 00:36:15 +0200

    Update to 2018 && AI PatrolGuard Fixes

---
+++ Assets/Scripts/AIController/FreeWayPointsController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeWayPointsController : MonoBehaviour {
+
+    public GameObject patrolPointParent;
+    public Transform[] patrolPoints;
+
+    private int lastWP = 0;
+    private int nextWP = 0;
+
+    [SerializeField]
+    private bool[] freeWP;
+
+    public static FreeWayPointsController wayPointsController;
+
+    void Awake()
+    {
+        //Unique GameManager
+        if (wayPointsController == null) wayPointsController = this;
+        else if (wayPointsController != this) Destroy(gameObject);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+
+        patrolPoints = patrolPointParent.GetComponentsInChildren<Transform>();
+        freeWP = new bool[patrolPoints.Length];
+        //first element is always false
+        for (int i = 1; i < freeWP.Length; i++)
+        {
+            freeWP[i] = true;
+        }
+    }
+	
+	public void SetFree(int free,bool state)
+    {
+        freeWP[free] = state;
+    }
+
+    public bool getFreeState(int free)
+    {
+        return freeWP[free];
+    }
+}
--- Assets/Scripts/AIController/PatrollingGuards.cs
+++ Assets/Scripts/AIController/PatrollingGuards.cs
@@ -12,14 +12,24 @@ public class PatrollingGuards : MonoBehaviour {
     public float minGuardingTime = 5f;
     public float maxGuardingTime = 10f;
 
+    //public FreeWayPointsController wayPointController;
+
 
     private NavMeshAgent navMeshAgent;
+    private NavMeshObstacle navMeshObstacle;
     private Transform nextTarget;
     private Transform currentTarget;
+
+    private int lastWP = 0;
+    private int nextWP = 0;
+
+    [SerializeField]
     private State currentState;
     private Animator animator;
     private int idleAnimation;
     private bool idleSet = false;
+    [SerializeField]
+    //private bool[] freeWP;
 
     enum State
     {
@@ -31,27 +41,43 @@ public class PatrollingGuards : MonoBehaviour {
     {
         patrolPoints = patrolPointParent.GetComponentsInChildren<Transform>();
         navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshObstacle = GetComponent<NavMeshObstacle>();
+        //wayPointController = GetComponentInParent<FreeWayPointsController>();
         currentState = State.Guarding;
         animator = GetComponent<Animator>();
-        animator.SetBool("Static_b", false);
+        animator.SetBool("Static_b", false);        
     }
 
 
 	// Update is called once per frame
 	void Update () {
-
+        //Obstacle Avoid Off Nav On
+        //navMeshObstacle.enabled = false;
+        //navMeshAgent.enabled = true;
         if (currentState == State.Guarding) {
-            if (guardingTime <= 0) nextGuardPoint();
+
+            if (guardingTime <= 0) NextGuardPoint(false);
             else
             {
                 guardingTime -= Time.deltaTime;
-                transform.localEulerAngles = new Vector3(nextTarget.transform.eulerAngles.x, nextTarget.transform.eulerAngles.y, nextTarget.transform.eulerAngles.z);
+                transform.localEulerAngles = new Vector3(nextTarget.transform.eulerAngles.x, nextTarget.transform.eulerAngles.y+90, nextTarget.transform.eulerAngles.z);                
             }      
-        }
-        else if(navMeshAgent.remainingDistance < 0.5f)
+        }else if(currentState == State.Patrolling)
         {
-            currentState = State.Guarding;
-            navMeshAgent.isStopped = true;
+            if(navMeshAgent.remainingDistance < 2.0f && navMeshAgent.remainingDistance >= 0.5f)
+            {
+                if (!FreeWayPointsController.wayPointsController.getFreeState(nextWP)) NextGuardPoint(true);
+            }
+            else if (navMeshAgent.remainingDistance < 0.5f)
+            {
+                //if (!FreeWayPointsController.wayPointsController.getFreeState(nextWP))NextGuardPoint(true);
+                //else
+                //{
+                    currentState = State.Guarding;
+                    navMeshAgent.isStopped = true;
+                //}
+            }
+            else navMeshAgent.isStopped = false;
         }
 
         if (!navMeshAgent.isStopped)
@@ -67,20 +93,35 @@ public class PatrollingGuards : MonoBehaviour {
             {
                 idleSet = true;
                 int animation = Random.Range(0, 3);
-                animator.SetInteger("Animation_int", animation);
+                animator.SetInteger("Animation_int", animation);                
             }
+            FreeWayPointsController.wayPointsController.SetFree(nextWP, false);
         }
     }
 
-    void nextGuardPoint()
+    void NextGuardPoint(bool wasSeated)
     {
-        int nextPoint = Random.Range(1, patrolPoints.Length);
-        print("WP: " + nextPoint);
-        nextTarget = patrolPoints[nextPoint];
-        navMeshAgent.SetDestination(nextTarget.position);
-        navMeshAgent.isStopped = false;
-        idleSet = false;
+        int nextPoint = 0;
+        while (true)
+        {
+            //Start with 1 for not using the parent transform
+            nextPoint = Random.Range(1, patrolPoints.Length);
+            print("WP: " + nextPoint);
+            nextTarget = patrolPoints[nextPoint];
+            nextWP = nextPoint;
+            if (nextTarget == currentTarget || FreeWayPointsController.wayPointsController.getFreeState(nextPoint) == true) break;            
+        }
+        if (nextTarget != currentTarget)
+        {
+            navMeshAgent.SetDestination(nextTarget.position);
+            navMeshAgent.isStopped = false;
+            idleSet = false;
 
+        }
+        //if(lastWP != nextWP && !wasSeated)
+        FreeWayPointsController.wayPointsController.SetFree(lastWP, true);
+        currentTarget = patrolPoints[nextPoint];        
+        lastWP = nextWP;
         guardingTime = Random.Range(minGuardingTime, maxGuardingTime);
         currentState = State.Patrolling;
     }
